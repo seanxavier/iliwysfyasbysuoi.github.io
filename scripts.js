7e964f731891a8a4f4dbebb7bc864817c9daa7cc
iliwysfyasbysuoi <sean_xavier_nieva@dlsu.edu.ph>
2022-04-04 02:28:57 +0800

    bug

--- scripts.js
+++ scripts.js
@@ -8,9 +8,6 @@ $(document).ready(function(){
 function SendEmail(){
     var subject = $("#emailsubject").val();
     var body = $("#emailbody").val();
-    alert(subject + body);
-
-
     window.location = `mailto:sean.xaviernieva@gmail.com?subject=${subject}&body=${body}`;
 }
 
